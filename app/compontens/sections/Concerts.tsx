73ec5c78aad5f2057dbcb7345f778240d96a1345
KUSY <frajeros@gmail.com>
2023-03-17 14:53:28 +0100

    add placeholder img

--- app/compontens/sections/Concerts.tsx
+++ app/compontens/sections/Concerts.tsx
@@ -14,7 +14,8 @@ const Concerts = () => {
             <div className="pt-12 pb-24 max-w-4xl mx-auto fsac4 md:px-1 px-3">
                 <Link href="http://www.bieszczadzka24.pl/aktualnosci/31-final-wielkiej-orkiestry-swiatecznej-pomocy-w-ustrzykach-dolnych-za-nami/7562" target={"_blank"}>
                     <div className="ktq4 shadow-2xl">
-                        <Image src={'/images/plakat_wosp_23.webp'} width={600} height={500} alt="plakat WOŚP w Ustrzykach Dolnych 2023" loading="lazy" />
+                        <Image src={'/images/plakat_wosp_23.webp'} width={600} height={500} alt="plakat WOŚP w Ustrzykach Dolnych 2023" loading="lazy" blurDataURL="https://placehold.co/600x500"
+                            placeholder="blur" />
                         <h3 className="pt-3 font-semibold text-lg text-white">
                             Koncert podczas Finału Wielkiej Orkiestry Świątecznej Pomocy
                         </h3>
@@ -22,7 +23,8 @@ const Concerts = () => {
                 </Link>
                 <Link href="https://www.stolicabieszczad.pl/ustrzycki-festiwal-rockowy-uwolnij-energie-z-gor.html" target={"_blank"}>
                     <div className="ktq4 shadow-2xl">
-                        <Image src='/images/plakat_ud_festiwal_22.webp' width={600} height={500} alt="Ustrzycki festiwal rockowy 2022" loading="lazy" />
+                        <Image src='/images/plakat_ud_festiwal_22.webp' width={600} height={500} alt="Ustrzycki festiwal rockowy 2022" loading="lazy" blurDataURL="https://placehold.co/600x500"
+                            placeholder="blur" />
                         <h3 className="pt-3 font-semibold text-lg text-white">
                             Ustrzycki Festiwal Rockowy 2022
                         </h3>
@@ -30,7 +32,8 @@ const Concerts = () => {
                 </Link>
                 <Link href="http://www.bieszczadzka24.pl/aktualnosci/dozynki-gminne-w-wojtkowej/7484" target={"_blank"}>
                     <div className="ktq4 shadow-2xl">
-                        <Image src='/images/plakat_dozynki_wojtkowa_22.webp' width={600} height={500} alt="Dożynki Gminne Wojtkowa 2022" loading="lazy" />
+                        <Image src='/images/plakat_dozynki_wojtkowa_22.webp' width={600} height={500} alt="Dożynki Gminne Wojtkowa 2022" loading="lazy" blurDataURL="https://placehold.co/600x500"
+                            placeholder="blur" />
                         <h3 className="pt-3 font-semibold text-lg text-white">
                             Dożynki Gminne Wojtkowa 2022
                         </h3>
@@ -38,7 +41,8 @@ const Concerts = () => {
                 </Link>
                 <Link href="https://wbieszczady.pl/aktualnosci/pozegnanie-z-koleda-w-ustrzykach-dolnych-koncert-zespolu-light-of-freedom-i-przyjaciele-oraz-ustrzyckiej-grupy-wokalnej-zdjecia/CPGWTYAt2br9KWByQt2J" target={"_blank"}>
                     <div className="ktq4 shadow-2xl">
-                        <Image src='/images/plakat_pozegnanie_koleda_23.webp' width={600} height={500} alt="plakat WOŚP w Ustrzykach Dolnych 2023" loading="lazy" />
+                        <Image src='/images/plakat_pozegnanie_koleda_23.webp' width={600} height={500} alt="plakat WOŚP w Ustrzykach Dolnych 2023" loading="lazy" blurDataURL="https://placehold.co/600x500"
+                            placeholder="blur" />
                         <h3 className="pt-3 font-semibold text-lg text-white">
                             Koncert Pożegnanie z kolędą w Ustrzyckim Domu Kultury 2023
                         </h3>
