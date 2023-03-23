9b4d4779dfd609b61a884a3e1976db6304c6d71b
KUSY <frajeros@gmail.com>
2023-03-23 08:20:44 +0100

    delete mp3 player

--- app/compontens/sections/Listen.tsx
+++ app/compontens/sections/Listen.tsx
@@ -1,27 +1,10 @@
-'use client';
-import React, { useRef } from 'react'
-
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+import React from 'react'
 
 import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
 
 const Listen = () => {
-    const jazzRef = useRef<AudioPlayer>(null);
-    const rockRef = useRef<AudioPlayer>(null);
-
-    const handleStopPlayer = (name: string) => {
-        console.log(rockRef?.current?.audio?.current)
-        if (name === 'jazz') {
-            rockRef?.current?.audio?.current?.pause();
-        }
-        if (name === 'rock') {
-            jazzRef?.current?.audio?.current?.pause();
-        }
-    }
-
     return (
         <section id="posluchaj">
             <div className="container mx-auto">
@@ -32,30 +15,6 @@ const Listen = () => {
                     Posłuchaj naszych sampli!
                 </p>
                 <div className='pt-12 pb-24 max-w-4xl mx-auto fsac4 md:px-1 px-3'>
-                    <div className="ktq4 shadow-2xl">
-                        <h3 className='text-white font-bold text-3xl mb-4 text-center'>Jazz</h3>
-                        <AudioPlayer
-                            preload="none"
-                            ref={jazzRef}
-                            autoPlay={false}
-                            onEmptied={() => console.log("test")}
-                            src="/musics/jazz.mp3"
-                            onPlay={() => handleStopPlayer('jazz')}
-                            showJumpControls={false}
-                            customAdditionalControls={[]}
-                        />
-                    </div>
-                    <div className="ktq4 shadow-2xl">
-                        <h3 className='text-white font-bold text-3xl mb-4 text-center'>Rockowa składanka</h3>
-                        <AudioPlayer
-                            ref={rockRef}
-                            autoPlay={false}
-                            src="/musics/rock_skladanka.mp3"
-                            onPlay={() => handleStopPlayer('rock')}
-                            showJumpControls={false}
-                            customAdditionalControls={[]}
-                        />
-                    </div>
                     <div className="ktq4 shadow-2xl">
                         <h3 className='text-white font-bold text-3xl mb-4 text-center'>Fragment z koncertu</h3>
                         <LiteYouTubeEmbed
