fcafb31b92cf23f20098e1fec7ebd743abe338ed
KUSY <frajeros@gmail.com>
2023-03-23 08:47:22 +0100

    loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,36 +49,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Suspense fallback={<div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'><svg version="1.1" id="L5" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-          viewBox="0 0 100 100" enableBackground="new 0 0 0 0" xmlSpace="preserve">
-          <circle fill="#fff" stroke="none" cx="6" cy="50" r="6">
-            <animateTransform
-              attributeName="transform"
-              dur="1s"
-              type="translate"
-              values="0 15 ; 0 -15; 0 15"
-              repeatCount="indefinite"
-              begin="0.1" />
-          </circle>
-          <circle fill="#fff" stroke="none" cx="30" cy="50" r="6">
-            <animateTransform
-              attributeName="transform"
-              dur="1s"
-              type="translate"
-              values="0 10 ; 0 -10; 0 10"
-              repeatCount="indefinite"
-              begin="0.2" />
-          </circle>
-          <circle fill="#fff" stroke="none" cx="54" cy="50" r="6">
-            <animateTransform
-              attributeName="transform"
-              dur="1s"
-              type="translate"
-              values="0 5 ; 0 -5; 0 5"
-              repeatCount="indefinite"
-              begin="0.3" />
-          </circle>
-        </svg></div>}>
+        <Suspense fallback={<div role="status" className='flex justify-center align-middle min-h-screen w-screen'>
+          <div className="w-12 h-12 rounded-full animate-spin border border-solid border-white border-t-transparent"></div>
+        </div>}>
           <HashScroll>
             {children}
             <Footer />
